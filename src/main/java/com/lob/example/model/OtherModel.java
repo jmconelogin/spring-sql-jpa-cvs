049727f136cdd08379c6c5e2d6b0fcc492c96222
Mike Sherry <mike.sherry@cvshealth.com>
2022-03-07 09:21:35 -0600

    Update Base Accellerator to be more generic

--- src/main/java/com/lob/example/model/OtherModel.java
+++ src/main/java/com/lob/example/model/OtherModel.java
@@ -1,16 +1,14 @@
-package com.example.users;
+package com.lob.example.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity // This tells Hibernate to make a table out of this class
 @Table(name="PRESCRIPTION") // USER is a reserved word for many SQL databases
-public class Prescription {
+public class OtherModel {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Integer prescription_id;
@@ -72,21 +70,5 @@ public class Prescription {
 	public void setInstructionCustomer(String instruction_to_customer) {
 		this.instruction_to_customer = instruction_to_customer;
 	}
-
-//	public Date getPrescriptionRefilledDate() {
-//		return prescription_refilled_date;
-//	}
-//
-//	public void setPrescriptionRefilledDate(Date prescription_refilled_date) {
-//		this.prescription_refilled_date = prescription_refilled_date;
-//	}
-//
-//	public Date getPrescriptionIssuedDate() {
-//		return prescription_issued_date;
-//	}
-//
-//	public void setPrescriptionIssuedDate(Date prescription_issued_date) {
-//		this.prescription_issued_date = prescription_issued_date;
-//	}
 
 }
--- src/main/java/com/lob/example/repository/OtherRepository.java
+++ src/main/java/com/lob/example/repository/OtherRepository.java
@@ -1,10 +1,12 @@
-package com.example.users;
+package com.lob.example.repository;
+
+import com.lob.example.model.OtherModel;
 
 import org.springframework.data.repository.CrudRepository;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface PrescriptionRepository extends CrudRepository<Prescription, Integer> {
+public interface OtherRepository extends CrudRepository<OtherModel, Integer> {
 
 }
